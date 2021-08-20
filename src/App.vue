c7a4ed770f02ef07bcc0adcd0018ddf3064c25c1
liuya <1522962872@qq.com>
2021-08-20 18:23:30 +0800

    element-plus 改成中文

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
-  <router-view></router-view>
+  <el-config-provider :locale="locale">
+    <router-view></router-view>
+  </el-config-provider>
 </template>
 
 <script>
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+import { ElConfigProvider } from 'element-plus'
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
 export default {
-  name: 'App'
+  components: {
+    [ElConfigProvider.name]: ElConfigProvider,
+  },
+  name: 'App',
+  data() {
+    return {
+      locale: zhCn,
+    }
+  }
 }
 </script>
 
